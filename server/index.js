cb84349389bb512e19fb853c0eabdd55e04e0ea3
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2025-09-29 13:46:58 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app=express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/health",(req,res)=>{
+    res.json({status:"ok", message:"Server is healthy"});
+})
+
+const PORT= process.env.PORT || 8080;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on POORT ${PORT}`);
+});
